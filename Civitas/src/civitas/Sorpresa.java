b4b3999a0109524cba69a3632a8863a4e175b1c5
jmnavas <joselillo.sk8@gmail.com>
2022-10-28 20:42:52 +0200

    New Sorpresa: atributos y metodos

--- Civitas/src/civitas/Sorpresa.java
+++ Civitas/src/civitas/Sorpresa.java
@@ -3,12 +3,38 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package civitas;
+import java.util.ArrayList;
 
 /**
- * Clase vacía creada para que no se generen errores en la clase
- * MazoSorpresas
  * @author jmnavas
  */
 public class Sorpresa {
+    private String texto;
+    private int    valor;
     
+    
+    void aplicarAJugador ( int actual, ArrayList <Jugador> todos ) {
+        
+    }
+    
+    private void aplicarAJugador_pagarCobrar ( int actual, ArrayList <Jugador> todos ) {
+        
+    }
+    
+    private void aplicarAJugador_porCasaHotel ( int actual, ArrayList <Jugador> todos ) {
+        
+    }
+    
+    private void informe ( int actual, ArrayList <Jugador> todos ) {
+        
+    }
+    
+    Sorpresa ( TipoSorpresa tipo, String texto, int valor ) {
+        
+    }
+    
+    @Override
+    public String toString ( ) {
+        return "\n Hola :3 ";
+    }
 }
